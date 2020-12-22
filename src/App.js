753d53694915f62c35c54838bee85d686561ea10
Arun <arunkumarofcl@gmail.com>
2020-12-22 10:28:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <a href="/">React shopping Cart</a>
       </header>
      <main>MAIN PRODUCT</main>
-     <footer>All rights is reserved</footer>
+     <footer>All rights is reserved.</footer>
     </div>
   );
 }
